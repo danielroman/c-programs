133280763c2218a48094fe8b31bf26f78a9f2eb4
daniel.roman.smr@gmail.com <dany@dany-System-Product-Name.(none)>
2014-03-19 21:10:01 +0100

    Ejercicios

--- solutions/09_power/09_power.cpp
+++ solutions/09_power/09_power.cpp
@@ -3,31 +3,53 @@
 
 #define N 0x10
 
-int power(int list[N], int exponet){
 
 
+
+void error(char *argv[]){
+
+    fprintf(stderr, "Usage: %s numero \n", argv[0]);
+    exit(EXIT_FAILURE);
 }
 
+//Rellenar una matriz con numeros de 1 a 16 ambos inclusive
 void fill_in(int list[N]){
-    for (int cont = 0; cont < N; cont++)
-	list[cont] = cont + 1;
+    for (int numeros = 0; numeros < N; numeros++)
+	list[numeros] = numeros + 1; 
+}
+
+//Elevar los numeros al exponente indicado
+int power(int base[N], int exponent){
+    
+    int valor;
+    for (int numeros = 0; numeros < N; numeros++){
+	valor = base[numeros];
+	for (int elevado = 0; elevado < exponent -1; elevado++)
+		base[numeros] *= valor;
+    }
 }
 
 
 int main(int argc, char *argv[]){
 
-    if(argc != 2)
-	error(argv[0]);
+    int exponent;
+    int base[N];
 
-    int list[N];
-    int exponent = atoi(argv[1]);
+    if (argc < 2)
+	error(argv);
 
-    fill_in(list, exponent);
+    exponent = atoi(argv[1]);
+       
+    fill_in(base);
+    power(base,exponent);
 
-    for(int contador=0; contador < N; contador++)
-	printf("%i\n", list[contador]);
+    
+//Imprimir el resultado de la multiplicacion
+    for (int cont = 0; cont < N; cont++)
+	printf("%i ", base[cont]); 
 
 
+    printf("\n");
     return EXIT_SUCCESS;
 
 }
