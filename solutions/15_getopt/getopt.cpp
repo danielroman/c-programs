d6c77cb9bc2406ea1791ed48889fc78e33aa251b
daniel.roman.smr@gmail.com <dany@dany-System-Product-Name.(none)>
2014-03-23 18:27:36 +0100

    Terminado

--- solutions/15_getopt/getopt.cpp
+++ solutions/15_getopt/getopt.cpp
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
 
 int
 main (int argc, char **argv)
 {
     const char *frase = "El resultado de la suma es:";
     const char *string = "The result of the adding is:";
-    const char *end = NULL;
+    const char *idioma = NULL;
     const char *result = NULL;
 
     int opciones;
@@ -20,11 +20,11 @@ main (int argc, char **argv)
 	switch (opciones)
 	{
 	    case 'l':
-		 end = optarg;
-		 if (*end == 's')
-		     result = frase;
-		 if (*end == 'e')
-		     result = string;
+		idioma = optarg;
+		if (!strcmp(idioma, "sphanis"))
+		    result = frase;
+		if (!strcmp(idioma, "english"))
+		    result = string;
 		break;
 	    case '?':
 		if (optopt == 'l')
@@ -40,10 +40,9 @@ main (int argc, char **argv)
 		abort ();
 	}
 
-  
+
     //if (result == "sphanis")
-	printf ("Result = %s\n",
-	    result);
+    printf ("%s \n", result);
 
     for (int index = optind; index < argc; index++)
 	printf ("Non-option argument %s\n", argv[index]);
