d7c66c5bb7b6fcab953fb1f00253a26943980a56
daniel.roman.smr@gmail.com <dany@dany-System-Product-Name.(none)>
2014-03-30 14:58:16 +0200

    Terminado

--- solutions/16_sort_array/sort_Array.cpp
+++ solutions/16_sort_array/sort_Array.cpp
@@ -5,27 +5,19 @@
 int main(int argc, char *argv[]){
     int A[] = {22, 3, 77, 50, 40, 23, 37, 35, 22, 2, 61, 71, 0};
     int cantidad_numeros = sizeof(A) / sizeof(int);
-    int veces = cantidad_numeros;
-    int aux[cantidad_numeros];
-    int contador = 0;
-    int contador1 = 0;
-    int contador2 = 0;
-    do{
 
-	for (int numero = 0; numero <= cantidad_numeros; numero++){
-	    for(int posicion = 1; posicion < cantidad_numeros; posicion++){
-		if(A[numero] < A[posicion]){
-		    //Conseguir guardar el valor en las diferentes posiciones del array ??
-			aux[contador] = A[numero];
-		}	    
-	    }
-	}
-	cantidad_numeros--;
-	contador++;
-    }while(contador < veces);
+    for (int numero = 0; numero <= cantidad_numeros; numero++){
+	int aux;
+	for(int posicion = numero + 1; posicion < cantidad_numeros; posicion++)
+	    if(A[numero] > A[posicion]){
+		aux = A[numero];
+		A[numero] = A[posicion];
+		A[posicion] = aux;
+	    }	    
+    }
 
-    for (int muestra = 0; muestra < veces; muestra++)
-	printf("%i ", aux[muestra]);
+    for (int muestra = 0; muestra < cantidad_numeros; muestra++)
+	printf("%i ", A[muestra]);
     printf("\n");
 
     return EXIT_SUCCESS;
