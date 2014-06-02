8973c0c764f03f4cac05503c6ed5d0d768dcad34
daniel.roman.smr@gmail.com <dany@dany-System-Product-Name.(none)>
2014-06-02 20:47:36 +0200

    Hecho tempororizador, falta colocarlo en el programa

--- juego/bomberman.cpp
+++ juego/bomberman.cpp
@@ -2,16 +2,25 @@
 #include <stdlib.h>
 #include <ncurses.h>
 #include <ctype.h>
+#include <unistd.h>
 
 #define FILAS 25
 #define COL 50
 #define ESC 27
+#define SPC 32
 
 //http://www.openvim.com/tutorial.html
 
 struct Coordenadas{
     int x;
     int y;
+    int bombas;
+};
+
+struct Bomba{
+    int x;
+    int y;
+    int explosion;
 };
 
 void mapa(char tablero[FILAS][COL]){
@@ -28,8 +37,6 @@ void mapa(char tablero[FILAS][COL]){
 		fscanf(fichero, " %c", &tablero[fila][col]);
 	fclose(fichero);
     }
-
-
 }
 
 void pintar_mapa(char tablero[FILAS][COL], struct Coordenadas jugador){
@@ -48,7 +55,6 @@ void pintar_mapa(char tablero[FILAS][COL], struct Coordenadas jugador){
 
 void inicializar(char tablero[FILAS][COL]){
 
-
     for (int fila = 0; fila < FILAS; fila++)
 	for (int col = 0; col < COL; col++)
 	    if (tablero[fila][col] == '1')
@@ -63,8 +69,61 @@ void inicializar(char tablero[FILAS][COL]){
 			if (tablero[fila][col] == '4')
 			    tablero[fila][col] = 'L';
 }
-void mover(int user_input, struct Coordenadas *jugador, char tablero[FILAS][COL]){
 
+//hay que mejorar el codigo chapuza momentanea
+void explotar_bomba(char tablero[FILAS][COL], struct Coordenadas *jugador){
+
+	    int alcanceizq = 0;
+	    int alcanceder = 0;
+	    int alcanceaba = 0;
+	    int alcancearr = 0;
+
+	    while(alcanceizq < 5){
+		if(tablero[jugador->y][jugador->x - alcanceizq] != 'T'){
+		    tablero[jugador->y][jugador->x - alcanceizq] = 'x';
+		    alcanceizq++;
+		}else 
+		    alcanceizq = 5;
+	    }
+
+	    while(alcanceder < 5){
+		if(tablero[jugador->y][jugador->x + alcanceder] != 'T'){
+		    tablero[jugador->y][jugador->x + alcanceder] = 'x';
+		    alcanceder++;
+		}else 
+		    alcanceder = 5;
+	    }
+	    
+	    while(alcancearr < 5){
+		if(tablero[jugador->y - alcancearr][jugador->x] != 'T'){
+		    tablero[jugador->y - alcancearr][jugador->x] = 'x';
+		    alcancearr++;
+		}else 
+		    alcancearr = 5;
+	    }
+		
+	    while(alcanceaba < 5){
+		if(tablero[jugador->y + alcanceaba][jugador->x] != 'T'){
+		    tablero[jugador->y + alcanceaba][jugador->x] = 'x';
+		    alcanceaba++;		   
+		}else 
+		    alcanceaba = 5;
+	    }
+}
+
+void poner_bomba(char tablero[FILAS][COL], struct Coordenadas *jugador){ 
+    if(jugador->bombas < 3)
+	if(tablero[jugador->y + 1][jugador->x] != 'T'){
+	     tablero[jugador->y + 1][jugador->x] = 'X'; 
+	     jugador->bombas++;
+	}
+    //realizar temporizador
+
+    //explotar_bomba(tablero, jugador);
+}
+
+
+void mover(int user_input, struct Coordenadas *jugador, char tablero[FILAS][COL]){
     switch(tolower(user_input)){
 	case 'w':
 	case KEY_UP:
@@ -94,6 +153,9 @@ void mover(int user_input, struct Coordenadas *jugador, char tablero[FILAS][COL]
 		jugador->x += 1;
 	    }
 	    break;	
+	case SPC:
+	    poner_bomba(tablero, jugador);
+	    break;
     }
     tablero[jugador->y][jugador->x] = 'U';
 }
@@ -101,11 +163,11 @@ void mover(int user_input, struct Coordenadas *jugador, char tablero[FILAS][COL]
 int main(int argc, char *argv[]){
 
     struct Coordenadas jugador = {1, 1}; //posicion inicial del jugador
-    struct Coordenadas movimiento = {0,0}; //no se movera
     char tablero[FILAS][COL];
     int user_input;
 
     initscr();
+    halfdelay(1);
     keypad(stdscr, TRUE);
     noecho();
     curs_set(0);
