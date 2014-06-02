8755811b4a477c195e614f2ea279e0a0cdc07f70
Daniel Fernandez Perez <daniel.fdz.perez@gmail.com>
2014-06-02 14:14:13 +0200

    hacer bombas

--- juego/bomberman.cpp
+++ juego/bomberman.cpp
@@ -5,6 +5,8 @@
 
 #define FILAS 25
 #define COL 50
+#define ESC 27
+
 
 struct Coordenadas{
     int x;
@@ -25,68 +27,74 @@ void mapa(char tablero[FILAS][COL]){
 		fscanf(fichero, " %c", &tablero[fila][col]);
 	fclose(fichero);
     }
+
+
 }
 
 void pintar_mapa(char tablero[FILAS][COL], struct Coordenadas jugador){
 
-    mapa(tablero);
+    clear();
     for(int fila = 0; fila < FILAS; fila++){
 	for (int col = 0; col < COL; col++){
+	    printw("%c", tablero[fila][col]);
 
-	    if(fila == jugador.y && col == jugador.x)
-		printf("%c", tablero[fila][col] = 'U');
+	}
+	printw("\n");
+    }
+    refresh();
 
+}
+
+void inicializar(char tablero[FILAS][COL]){
+
+
+    for (int fila = 0; fila < FILAS; fila++)
+	for (int col = 0; col < COL; col++)
 	    if (tablero[fila][col] == '1')
-		printf("%c",tablero[fila][col] = 'T');
-
-	    if(tablero[fila][col] == '0')    
-		printf("%c", tablero[fila][col] = ' ');	
-
-	    if (tablero[fila][col] == '2')
-		printf("%c", tablero[fila][col] = ' ');
-	}
-    printf("\n");
-    }
-
+		tablero[fila][col] = 'T';
+	    else
+		if(tablero[fila][col] == '0')    
+		    tablero[fila][col] = ' ';
+		else 	    
+		    if (tablero[fila][col] == '2')
+			tablero[fila][col] = 'I';
+		    else
+			if (tablero[fila][col] == '4')
+			    tablero[fila][col] = 'L';
 }
+void mover(int user_input, struct Coordenadas *jugador, char tablero[FILAS][COL]){
 
-void mover(struct Coordenadas movimiento, struct Coordenadas jugador){
-
-
-
-}
-
-
-void mover(int user_input, struct Coordenadas movimiento, struct Coordenadas jugador, char tablero[FILAS][COL]){
-
-    while((user_input = getch()) != ESC){
-
-	switch(tolower(user_input)){
-	    case 'w':
-	    case KEY_UP:
-		if (tablero[jugador.y + 1][jugador.x] != 'T'){
-		movimiento.x = 0;
-		movimiento.y = -1;
-
-		}
-		break;
-	    case 's':
-	    case KEY_DOWN:
-		movimiento.x = 0;
-		movimiento.y = -1;
-		break;
-            case 'a':
-	    case KEY_LEFT:
-		movimiento.x = -1;
-		movimiento.y = 0;
-		break;
-	    case 'd':
-	    case KEY_RIGHT:
-		movimiento.x = +1;
-		movimiento.y = 0;
-		break;	
-	}
+    switch(tolower(user_input)){
+	case 'w':
+	case KEY_UP:
+	    if (tablero[jugador->y - 1][jugador->x] != 'T'){
+		tablero[jugador->y][jugador->x] = ' ';
+		jugador->y += -1;
+	    }
+	    break;
+	case 's':
+	case KEY_DOWN:
+	    if (tablero[jugador->y + 1][jugador->x] != 'T'){
+		tablero[jugador->y][jugador->x] = ' ';
+		jugador->y += 1;
+	    }
+	    break;
+	case 'a':
+	case KEY_LEFT:
+	    if (tablero[jugador->y][jugador->x - 1] != 'T'){
+		tablero[jugador->y][jugador->x] = ' ';
+		jugador->x += -1;
+	    }
+	    break;
+	case 'd':
+	case KEY_RIGHT:
+	    if (tablero[jugador->y][jugador->x + 1] != 'T'){
+		tablero[jugador->y][jugador->x] = ' ';
+		jugador->x += 1;
+	    }
+	    break;	
     }
+    tablero[jugador->y][jugador->x] = 'U';
 }
 
 int main(int argc, char *argv[]){
@@ -95,13 +103,17 @@ int main(int argc, char *argv[]){
     struct Coordenadas movimiento = {0,0}; //no se movera
     char tablero[FILAS][COL];
     int user_input;
-    pintar_mapa(tablero, jugador);
 
     initscr();
     keypad(stdscr, TRUE);
     noecho();
     curs_set(0);
-  //  movimiento_jugador(user_input, movimiento, jugador);
+    mapa(tablero);
+    inicializar(tablero);
+    while ((user_input = getch()) != ESC){	
+	mover(user_input, &jugador, tablero);
+	pintar_mapa(tablero, jugador);
+    }
     endwin();
     return EXIT_SUCCESS;
 
